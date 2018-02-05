d4ca198a93f9e3c9dc592098c523d2fcbb6f6f0e
RafaGM <rafa.guardeno.marquez@gmail.com>
2018-02-05 12:27:49 +0100

    Add files via upload

---
+++ objeto.js
@@ -0,0 +1,48 @@
+/***
+Se deben crear los siguientes objetos con sus atributos:
+
+Objeto Alumno:
+-Nombre
+-Apellidos
+-Email
+-Edad
+-Array de Cursos
+-Contraseña
+- nombreCompleto --> function
+
+
+Objeto Profesor:
+- Nombre
+-Apellidos
+-Email
+-Array de Cursos
+-Especialidad
+-Contraseña
+- nombreCompleto --> function
+*****/
+
+var Alumno = {
+	Nombre: "nombre",
+	Apellidos: ["Apellido1", "Apellido2"],
+	Email: "email@email.com",
+	Edad: 22,
+	Cursos: ["html", "css", "js"],
+	Contraseña: "********",
+	nombreCompleto : function() {
+		return this.Nombre + " "+ this.Apellidos[0] + " " + this.Apellidos[1];
+			}
+
+};
+
+var Profesor = {
+	Nombre: "nombre",
+	Apellidos: ["Apellido1", "Apellido2"],
+	Email: "email@email.com",
+	Cursos: ["html", "css", "js"],
+	Especialidad: "Suspender",
+	Contraseña: "********",
+	nombreCompleto : function() {
+		return this.Nombre + " "+ this.Apellidos[0] + " " + this.Apellidos[1];
+			}
+
+};
